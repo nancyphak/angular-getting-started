448b77d7c38d5fd7137d152e683935041e698ced
nancy phan <nancy.phan@eztek.vn>
2017-03-12 09:24:01 +0700

    add missing file

---
+++ client/src/appConfig.ts
@@ -0,0 +1,8 @@
+let appConfig = {
+    baseUrlApi: "http://localhost:8274/api/",
+    routeApi: {
+        getCategories: "categories",
+        createCategory: "categories"
+    }
+}
+export default appConfig;

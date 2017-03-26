22adfbddb5c068b9b18150e93f4f20ec68abff56
nancy phan <nancy.phan@eztek.vn>
2017-03-26 14:31:22 +0700

    implement http into ioc client

--- client/src/httpConnector.ts
+++ client/src/httpConnector.ts
@@ -5,25 +5,30 @@ import { Promise, PromiseFactory } from "./promise";
 import "rxjs/add/operator/map";
 import { Observable } from "rxjs/observable";
 import appConfig from "./appConfig";
+import appHelper from "./appHelper";
 // @Injectable()
 export class HttpConnector implements IConnector {
     // private http: Http = null;
     // constructor(http: Http) {
     //     this.http = http;
     // }
-    constructor(){}
+    constructor() { }
     public get(url: string): Promise {
         let def = PromiseFactory.create();
         let route = appConfig.rootApiUrl + url;
-        // this.http.get(route).map(this.handlePromise).subscribe(
-        //     (data: any) => def.reslove(data),
-        //     (error: any) => def.reject(error))
-        def.reslove([{ id: 1, name: "aa", description: "aa", photo: "photo.jpg", type: "aa" }]);
+        route = "http://localhost:3000/api/categories.json";
+
+        let http: Http = appHelper.injector.get(Http);
+        http.get(route).map(this.handlePromise).subscribe(
+            (data: any) => def.reslove(data),
+            (error: any) => def.reject(error))
+        // def.reslove([{ id: 1, name: "aa", description: "aa", photo: "photo.jpg", type: "aa" }]);
         return def;
     }
     public post(url: string, data: any): Promise {
         let def = PromiseFactory.create();
         let route = appConfig.rootApiUrl + url;
+        route = "http://localhost:3000/api/";
         // this.http.post(route, data).map(this.handlePromise).subscribe(
         //     (data: any) => def.reslove(data),
         //     (error: any) => def.reject(error)
--- client/src/main.ts
+++ client/src/main.ts
@@ -2,6 +2,9 @@
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 import { SecurityModule } from "./securityModule";
 import IocHelper from "./iocHelper";
+import appHelper from "./appHelper";
 IocHelper.configIoc().then(() => {
-    platformBrowserDynamic().bootstrapModule(SecurityModule);
+    platformBrowserDynamic().bootstrapModule(SecurityModule).then((platform: any) => {
+        appHelper.setInjector(platform.injector);
+    });
 })
